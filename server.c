e18d2077b915f43e0f38e1e42826f39a38e2ab76
Noah Hodgson <noahmhodgson@gmail.com>
2021-05-02 18:16:26 -0400

    added ack check back to where it was before

--- server.c
+++ server.c
@@ -184,16 +184,17 @@ int main(int argc, char* argv[])
 					//clearBuf(net_buf);
 					//printf("Waiting for datagram ack w/ seq: %d", seq);
 					packets_transmitted++;
+					//moved back
+					recvfrom(sockfd, &ack_buf, 1, sendrecvflag, (struct sockaddr*)&addr_con, &addrlen);
+					if(ack_buf == (char)seq){
+						wait = 0;
+						printf("\n DATAGRAM ACK RECIEVED\n");
+					}
 				}else{
 					printf("Packet Lost!\n");
 					dropped_packets++;
 				}
 				clearBuf(net_buf);
-				recvfrom(sockfd, &ack_buf, 1, sendrecvflag, (struct sockaddr*)&addr_con, &addrlen);
-				if(ack_buf == (char)seq){
-					wait = 0;
-					printf("\n DATAGRAM ACK RECIEVED\n");
-				}
 			}
 		}
 		if (fp != NULL)
