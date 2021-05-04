ed8c699d7b2ff7a18a20a078d314f44b458201f3
Noah Hodgson <noahmhodgson@gmail.com>
2021-05-03 21:54:05 -0400

    am i stupid?

--- client.c
+++ client.c
@@ -171,10 +171,9 @@ int main(int argc, char* argv[]){
 					if(!sim_ack_loss(ack_loss_rate)){
 						ack_buf = buffer_ack();//pull seq id
 						printf("\nAck buf = %d\n", ack_buf);
-						char* readin = (char*) malloc(82*sizeof(char));
+						char* readin = (char*) malloc(81*sizeof(char));
 						readin = strip_header(net_buf);
-						readin[81] = '\0';
-						readin[82] = '\0';
+						readin[80] = '\0';
 						printf(readin);
 						fputs(readin, fp); //parse datagram
 						sendto(sockfd, &ack_buf, 1, sendrecvflag, (struct sockaddr*)&addr_con, addrlen);//ack with seq number
