242f944610cf9d669de314ba39e74701bc0cfe0e
Noah Hodgson <noahmhodgson@gmail.com>
2021-05-07 23:23:00 -0400

    print statement client

--- client.c
+++ client.c
@@ -152,6 +152,7 @@ int main(int argc, char* argv[]){
 				done_flag = 1;
 				sendto(sockfd, &ack_seq, 1, sendrecvflag, (struct sockaddr*)&addr_con, addrlen);
 				good_acks++;
+				packs_received++;
 				printf("\nAck %d generated for transmission\n", ack_seq);
 				goto Skip;
 			}
